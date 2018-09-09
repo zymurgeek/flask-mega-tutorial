cb845d5b98b7a1eb807b6ca4a0aed74134f2ea5f
David Greenbaum <zymurgeek@gmail.com>
2018-09-08 20:06:48 -0400

    Implement chapter 4
    * Add SQLAlchemy and Flask-Migrate.
    * Add SQLAlchemy model.
    * Add users and posts table migrations.
    * Add Grip to view MD files.

--- microblog/microblog.py
+++ microblog/microblog.py
@@ -1,1 +1,9 @@
 from app import app
+from app import db
+from app.models import Post
+from app.models import User
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
