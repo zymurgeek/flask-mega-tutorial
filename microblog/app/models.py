54f0f02a71d2ab64bb80cfc4d430a72e66db6c1d
David A. Greenbaum <zymurgeek@gmail.com>
2018-09-08 23:07:27 -0400

    * Add flask-login to user model

--- microblog/app/models.py
+++ microblog/app/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
+from flask_login import UserMixin
 from werkzeug.security import check_password_hash
 from werkzeug.security import generate_password_hash
 
 from app import db
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
     email = db.Column(db.String(120), index=True, unique=True)
